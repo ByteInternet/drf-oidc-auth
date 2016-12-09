972854ec70eccae81fd40322056496a2969b776d
Allard Hoeve <allardhoeve@gmail.com>
2016-12-09 14:23:41 +0100

    Bump version to 0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='drf-oidc-auth',
-    version='0.8',
+    version='0.9',
     packages=['oidc_auth'],
     url='https://github.com/ByteInternet/drf-oidc-auth',
     license='MIT',
