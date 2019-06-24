ece861105830846031ee4891e03c0fec46edc04f
Alexander Dutton <adutton@oni.bio>
2019-06-24 09:57:31 +0100

    fixup! Use the built-in Django cache instead of a bespoke one.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,13 @@
 from random import random
 from unittest import TestCase
+
+from oidc_auth.settings import api_settings
 from oidc_auth.util import cache
+
+try:
+    from unittest.mock import patch, Mock, ANY
+except ImportError:
+    from mock import patch, Mock, ANY
 
 
 class TestCacheDecorator(TestCase):
@@ -39,3 +46,37 @@ class TestCacheDecorator(TestCase):
     def test_that_cache_can_store_None(self):
         self.assertIsNone(self.return_none())
         self.assertIsNone(self.return_none())
+
+    @patch('oidc_auth.util.caches')
+    def test_uses_django_cache_uncached(self, caches):
+        caches['default'].get.return_value = None
+        self.mymethod()
+        caches['default'].get.assert_called_with('oidc_auth.mymethod', version=1)
+        caches['default'].set.assert_called_with('oidc_auth.mymethod', ANY, timeout=1, version=1)
+
+    @patch('oidc_auth.util.caches')
+    def test_uses_django_cache_cached(self, caches):
+        return_value = random()
+        caches['default'].get.return_value = return_value
+        self.assertEqual(return_value, self.mymethod())
+        caches['default'].get.assert_called_with('oidc_auth.mymethod', version=1)
+        self.assertFalse(caches['default'].set.called)
+
+    @patch.object(api_settings, 'OIDC_CACHE_NAME', 'other')
+    def test_respects_cache_name(self):
+        caches = {
+            'default': Mock(),
+            'other': Mock(),
+        }
+        with patch('oidc_auth.util.caches', caches):
+            self.mymethod()
+            self.assertTrue(caches['other'].get.called)
+            self.assertFalse(caches['default'].get.called)
+
+    @patch.object(api_settings, 'OIDC_CACHE_PREFIX', 'some-other-prefix.')
+    @patch('oidc_auth.util.caches')
+    def test_respects_cache_prefix(self, caches):
+        caches['default'].get.return_value = None
+        self.mymethod()
+        caches['default'].get.assert_called_once_with('some-other-prefix.mymethod', version=1)
+        caches['default'].set.assert_called_once_with('some-other-prefix.mymethod', ANY, timeout=1, version=1)
