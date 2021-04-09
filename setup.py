7d7334e7abb309b6b66ede209da5928d05a1484d
Bono de Visser <bono@byte.nl>
2021-04-09 14:45:22 +0200

    Major version upgrade, because python3.4 support drop

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='drf-oidc-auth',
-    version='1.0.1',
+    version='2.0.0',
     packages=['oidc_auth'],
     url='https://github.com/ByteInternet/drf-oidc-auth',
     license='MIT',
