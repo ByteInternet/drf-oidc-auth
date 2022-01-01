03809c98eb8917a97954771e846e13761e13d733
Thore <thore@kruess.xyz>
2022-01-01 18:36:03 +0100

    Remove all eol python/django envs

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     install_requires=[
         'authlib>=0.15.0',
         'cryptography>=2.6',
-        'django>=1.8.0',
-        'djangorestframework>=3.0.0',
+        'django>=2.2.0',
+        'djangorestframework>=3.11.0',
         'requests>=2.20.0'
     ],
     classifiers=[
@@ -24,13 +24,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Internet',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Security',
