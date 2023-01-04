203ae03da5fb28dc41756a948462630c69f927d9
Simon Oliver Tveit <simtve@uninett.no>
2023-01-04 17:41:46 +0100

    Allow multiple JWT issuers

--- oidc_auth/authentication.py
+++ oidc_auth/authentication.py
@@ -7,8 +7,8 @@ from authlib.jose.errors import (BadSignatureError, DecodeError,
                                  ExpiredTokenError, JoseError)
 from authlib.oidc.core.claims import IDToken
 from authlib.oidc.discovery import get_well_known_url
+import jwt as pyjwt
 from django.utils.encoding import smart_str
-from django.utils.functional import cached_property
 from django.utils.translation import gettext as _
 from requests import request
 from requests.exceptions import HTTPError
@@ -86,25 +86,25 @@ class JSONWebTokenAuthentication(BaseAuthentication):
 
     www_authenticate_realm = 'api'
 
-    @property
     @cache(ttl=api_settings.OIDC_BEARER_TOKEN_EXPIRATION_TIME)
-    def oidc_config(self):
+    def oidc_config(self, oidc_endpoint):
         return requests.get(
             get_well_known_url(
-                api_settings.OIDC_ENDPOINT,
+                oidc_endpoint,
                 external=True
             )
         ).json()
 
-    @property
-    def claims_options(self):
+    def claims_options(self, issuer):
         _claims_options = {
             'iss': {
                 'essential': True,
-                'values': [self.issuer]
+                'values': [issuer]
             }
         }
-        for key, value in api_settings.OIDC_CLAIMS_OPTIONS.items():
+        issuer_config = self.get_issuer_config(issuer)
+        issuer_options = issuer_config['OIDC_CLAIMS_OPTIONS']
+        for key, value in issuer_options.items():
             _claims_options[key] = value
         return _claims_options
 
@@ -136,28 +136,28 @@ class JSONWebTokenAuthentication(BaseAuthentication):
 
         return auth[1]
 
-    def jwks(self):
-        return JsonWebKey.import_key_set(self.jwks_data())
+    def jwks(self, oidc_endpoint):
+        oidc_config = self.oidc_config(oidc_endpoint)
+        jwks_uri = oidc_config['jwks_uri']
+        return JsonWebKey.import_key_set(self.jwks_data(jwks_uri))
 
     @cache(ttl=api_settings.OIDC_JWKS_EXPIRATION_TIME)
-    def jwks_data(self):
-        r = request("GET", self.oidc_config['jwks_uri'], allow_redirects=True)
+    def jwks_data(self, jwks_uri):
+        r = request("GET", jwks_uri, allow_redirects=True)
         r.raise_for_status()
         return r.json()
 
-    @cached_property
-    def issuer(self):
-        return self.oidc_config['issuer']
-
     def decode_jwt(self, jwt_value):
         try:
+            issuer = self.get_issuer_from_raw_token(jwt_value)
+            key = self.get_key_for_issuer(issuer)
             id_token = jwt.decode(
                 jwt_value.decode('ascii'),
-                self.jwks(),
+                key,
                 claims_cls=IDToken,
-                claims_options=self.claims_options
+                claims_options=self.claims_options(issuer)
             )
-        except (BadSignatureError, DecodeError):
+        except (BadSignatureError, DecodeError, pyjwt.exceptions.DecodeError):
             msg = _(
                 'Invalid Authorization header. JWT Signature verification failed.')
             logger.exception(msg)
@@ -181,6 +181,28 @@ class JSONWebTokenAuthentication(BaseAuthentication):
         except JoseError as e:
             msg = _(str(type(e)) + str(e))
             raise AuthenticationFailed(msg)
+
+    def get_key_for_issuer(self, issuer):
+        issuer_config = self.get_issuer_config(issuer)
+        oidc_endpoint = issuer_config['OIDC_ENDPOINT']
+        key = self.jwks(oidc_endpoint)
+        return key
+
+    def get_issuer_from_raw_token(self, token):
+        claims = self.get_claims_without_validation(token)
+        if 'iss' not in claims:
+            raise AuthenticationFailed("Token is missing the 'iss' claim")
+        return claims['iss']
+
+    def get_claims_without_validation(self, token):
+        """Raises pyjwt.exceptions.DecodeError if token could not be decoded"""
+        return pyjwt.decode(token, options={"verify_signature": False})
+
+    def get_issuer_config(self, target_issuer):
+        issuer = api_settings.JWT_ISSUERS.get(target_issuer)
+        if not issuer:
+            raise AuthenticationFailed("Invalid 'iss' claim")
+        return issuer
 
     def authenticate_header(self, request):
         return 'JWT realm="{0}"'.format(self.www_authenticate_realm)
